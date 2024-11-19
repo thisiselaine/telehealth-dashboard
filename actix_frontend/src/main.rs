8f06133499e2d6be4b55aff4723b2601de0911e0
Elaine Ly <elainely2003@gmail.com>
2024-11-18 20:29:14 -0500

    bootstrap changes + frontend changes

--- actix_frontend/src/main.rs
+++ actix_frontend/src/main.rs
@@ -1,5 +1,6 @@
 use actix_files as fs; // For static file serving
 use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use serde_json::json;
 
 async fn api_handler() -> impl Responder {
     HttpResponse::Ok().json(serde_json::json!({ "message": "Hello from API" }))
